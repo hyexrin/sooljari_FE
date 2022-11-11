8bf2cf49b5a0a26fb66c345986375b561410897e
hyexrin <ohr1003@gmail.com>
2022-11-11 18:14:25 +0900

    HR 22-11-11 주류기록달력 UI 구현 完 >> DB 연동 예정

--- src/page/Main.js
+++ src/page/Main.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Recommend from '../component/Recommend'
 
+
 const main = ({product}) => {
     return (
         <div className='main-box'>
             <div className='event-box'>
-                <img src='https://cdn.pixabay.com/photo/2016/11/29/15/05/drink-1870139_960_720.jpg' className='event-img'/>
+                <img src='http://localhost:30/images/realmain.png' className='event-img'/>
             </div>
 
             <div className='recommend'>
